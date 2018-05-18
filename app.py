c2ef98e5b1bbfc0f256150dcaa44a0278ed09756
chriseisbrown <chriseisbrown@gmail.com>
2018-05-18 17:20:03 +0100

    add tokenise method

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello numpty!"
+
+
+# endpoint to perform a tokenisation
+@app.route("/tokenise", methods=["POST"])
+def tokenise():
+    value = request.form['value']
+    return value
 
 
 if __name__ == '__main__':
